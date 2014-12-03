7a8539a06165705aa013c5b30b3f908af949e31f
parvathyv <paro_v@yahoo.com>
2014-12-03 15:17:03 -0500

    Added test for no record found in the query for individual recipes. Passed all tests

--- models/recipe.rb
+++ models/recipe.rb
@@ -3,7 +3,7 @@ class Recipe
 
 	attr_reader :id, :name, :instructions, :description, :ingredients
   
-    def initialize(id, name, instructions, description, ingredients = nil)
+    def initialize(id = nil, name = nil, instructions = nil, description = nil, ingredients = nil)
     	@id = id
     	@name = name
     	@instructions = instructions
@@ -29,13 +29,16 @@ class Recipe
 
 	def self.find(id)
       query = "SELECT id, name, instructions, description FROM recipes where id = $1;"
-  		result1 = db_connection do |conn|
+  		result = db_connection do |conn|
     	 conn.exec_params(query,[id])
   		end 
-     
-      return Recipe.new(result1.to_a.first["id"], result1.to_a.first["name"], result1.to_a.first["instructions"], 
-        result1.to_a.first["description"],Ingredient.ingredients(id))
+      
+      if result.to_a.empty?
+        return Recipe.new(nil, nil, "This recipe doesn't have any instructions.", "This recipe doesn't have a description.",nil)
+        else
 
+       return Recipe.new(result.to_a.first["id"], result.to_a.first["name"], result.to_a.first["instructions"], result.to_a.first["description"],Ingredient.ingredients(id))
+     end 
   end	
       
     def self.each
